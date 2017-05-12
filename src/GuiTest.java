c99249409c08d2f964f2d3b05bd420b545ea7a62
M4kP4w31 <m4kp4w31@gmail.com>
2017-05-12 16:16:29 +0200

    InteliJ GUI form test

---
+++ src/GuiTest.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+class GuiTest extends JFrame {
+  private JPanel panel1;
+  private JButton button1;
+  private JButton button2;
+  private JButton button3;
+  private JTextField textField1;
+
+  private GuiTest() {
+    ActionListener buttonListener = (ActionEvent e) -> textField1.setText(((JButton)e.getSource()).getText());
+
+    button1.addActionListener(buttonListener);
+    button2.addActionListener(buttonListener);
+    button3.addActionListener(buttonListener);
+
+    add(panel1);
+  }
+
+  public static void main(String[] args) {
+    SwingConsole.run(new GuiTest(), 300, 100);
+  }
+}

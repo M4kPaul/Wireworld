8a69d8b93d577430800e0dbb9f7152e40a46d9fb
M4kP4w31 <m4kp4w31@gmail.com>
2017-05-14 03:09:33 +0200

    Replace with single class imports

--- src/Gui.java
+++ src/Gui.java
@@ -1,4 +1,5 @@
-import javax.swing.*;
+import javax.swing.JButton;
+import javax.swing.JPanel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import javax.swing.JFrame;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.WindowConstants;
-import java.awt.*;
+import java.awt.EventQueue;
 
 class Main {
   public static void main(String[] args) {
